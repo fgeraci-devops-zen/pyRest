739341bfe386652f9fed8403daa4f98f7480e62e
fgeraci-devops-zen <fgeraci@devops-zen.com>
2017-08-05 16:45:31 +0200

    Fixed regions

--- boto-aws.py
+++ boto-aws.py
@@ -1,9 +1,12 @@
 import boto3
-ec2client = boto3.client('ec2')
-response = ec2client.describe_instances()
-for reservation in response["Reservations"]:
-    for instance in reservation["Instances"]:
-        # This sample print will output entire Dictionary object
-        print(instance)
-        # This will print will output the value of the Dictionary key 'InstanceId'
-        print(instance["InstanceId"])
+
+access_key = "AKIAJZD2NFAY3PYHIQ3A"
+secret_key = "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"
+
+print("Stato ambiente dev: ")
+client = boto3.client('ec2')
+conn = boto3.resource('ec2')
+instances = conn.instances.filter()
+for instance in instances:
+    if instance.state["Name"] == "running":
+        print (instance.id, instance.state, instance.instance_type)
